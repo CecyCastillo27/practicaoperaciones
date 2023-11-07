84066178ed06909c3bc72f086bc99e92fb4e55eb
Cecilia isabel zuñiga c <132523018+CecyCastillo27@users.noreply.github.com>
2023-11-06 19:14:31 -0600

    Add files via upload

---
+++ Calcular/js/opera.js
@@ -0,0 +1,39 @@
+function sumar(){
+    let num1,num2, sumar
+    num1= prompt("Introduce un numero")
+    num2= prompt("Introduce otro numero")
+
+    sumar= parseInt(num1)+parseInt(num2)
+        alert(sumar)
+    
+}
+
+function restar(){
+    let num1,num2, restar
+    num1= prompt("Introduce un numero")
+    num2= prompt("Introduce otro numero")
+
+    restar= parseInt(num1)-parseInt(num2)
+        alert(restar)
+    
+}
+
+function multiplicar(){
+    let num1,num2, multiplicar
+    num1= prompt("Introduce un numero")
+    num2= prompt("Introduce otro numero")
+
+    multiplicar= parseInt(num1)*parseInt(num2)
+        alert(multiplicar)
+    
+}
+
+function dividir(){
+    let num1,num2, dividir
+    num1= prompt("Introduce un numero")
+    num2= prompt("Introduce otro numero")
+
+    dividir= parseInt(num1)/parseInt(num2)
+        alert(dividir)
+    
+}
